152e53db56485bf424c07d98419bd27bd70b141e
Juan Jerrah Barreto de Oliveira <juan.oliveira@avonale.com.br>
2022-05-28 19:30:12 -0300

    Aplicação Finalizada

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, View, Text } from 'react-native';
+import CustomImage from './components/CustomImage';
+
+import imagemLocal from '../assets/imgLocalImport.jpg'
+const imagemRemotaURI = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfPvfpzmaSagboeJJzEPwW4Dc04iHVRjKLmQ&usqp=CAU";
+
+export default function App() {
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <View>
+        <Text style={styles.titulo}>Três formas de Inserir Imagens</Text>
+        <CustomImage
+          fromWeb={false}
+          image={require('../assets/imgLocalRequire.jpg')}
+          title={"Imagem Local com 'require'"}
+        />
+        <CustomImage
+          fromWeb={false}
+          image={imagemLocal}
+          title={"Imagem Local com 'import'"}         
+        />
+        <CustomImage
+          fromWeb={true}
+          image={imagemRemotaURI}
+          title={"Imagem remota com 'URI'"}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#002baf',
+    alignItems: 'center',
+  },
+  titulo:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#fff',
+    marginBottom: 30
+  },
+});
---
+++ src/components/CustomImage.js
@@ -0,0 +1,42 @@
+import react from "react";
+import { Text, StyleSheet, Image, View } from 'react-native';
+
+export default function CustomImage({fromWeb, image, title}){
+    return(
+        <>
+            <View style={styles.bgImg} blurType={'light'} blurAmount={20}>
+                <Text style={styles.title}>
+                    {title}
+                </Text>
+                {fromWeb || <View ><Image source={image} style={styles.imageStyle}/></View>}
+                {!fromWeb || <View ><Image source={{uri: image}} style={styles.imageStyle}/></View>}
+                <View style={styles.circle}></View>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    title:{
+        paddingTop: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#fff',
+        paddingBottom: 10
+    },
+    bgImg:{
+        width: 250,
+        height: 240,
+        backgroundColor: 'red',
+        alignSelf: 'center',
+        marginBottom: 10,
+        borderRadius: 10,
+    },
+    imageStyle:{
+        alignSelf: 'center',
+        borderRadius: 10, 
+        width: 213,
+        height: 171,
+    },
+})
